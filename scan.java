f31cf15192ed41d191708d08aa282bb2bcefdf42
Shamture <ture.sham@gmail.com>
2016-08-24 14:52:28 +0530

    Add files via upload
    Scanner Java Class

---
+++ scan.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class scan
+	{
+	public static void main(String args[])
+		{	
+			Scanner sc=new Scanner(System.in);
+			String i =sc.nextLine();
+			System.out.println("The Scanner value of i is :"+i);
+}
+}
+	
